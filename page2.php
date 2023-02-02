4b9807a1f68e7f1269fb462099f0b28bc6d987a8
LeSachimi <91976776+LeSachimi@users.noreply.github.com>
2023-02-02 09:16:23 +0100

    push

--- page2.php
+++ page2.php
@@ -6,19 +6,9 @@
         <title>Un site plutôt sympa</title>
     </head>
     <body>
-        <h1 id="titre">Bienvenue sur le site de Sacha Bellet</h1>
+        <?php "<h1 id="titre">Bienvenue".$_GET['prenom'].$_GET['nom']."sur le site de Sacha Bellet</h1>"?>
 
         <h2>Un site fait à l'occasion d'un projet en classe: Ma première page web</h2>
-
-        <form method="get" action="page2.php">
-            <label>NOM : </label>
-            <input type="text" name="nom"> <br>
-            <label>Prénom : </label>
-            <input type="text" name="prenom"> <br>
-
-            <input type="submit" href="index.php" value="Envoyer">
-        </form>
-
         <h3>Le lieu ou j'étudie :</h3>
     <br/>
         <p>
